c4cbde462b6b35dcf6d2d88cd40dc257804fc706
peihsuan1225 <jessica861225@gmail.com>
2024-07-29 10:06:00 +0800

    # Correct format for db_port

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ posts = []
 
 DATABASE = {
     'host': os.getenv('DB_HOST'),
-    'port': int(os.getenv('DB_PORT')),
+    'port':3306,
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'db': os.getenv('DB_NAME'),
